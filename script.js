4bf658eb4a67943df1eaa42bd17d7fbde0b3f587
Rafael Silva de Souza <73540128+Rafael-Souz@users.noreply.github.com>
2022-02-01 17:56:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//operadores Aritméticos
+var a = 5 + 3;//=8
+var b = a % 5;//2
+var c = 5 * b ** 2;//45
+var d = 10 - a / 2;//6
+var e = 6* 2/ d;//2
+var f = (b % e) + (4/e);//3
+document.write('Igual =' + f )
+
+//operadores relacionais
+var a = 5 > 2;//true
+var b = 7 < 4;//false
+var c = 8 >= 8;//true
+var d = 9 <= 7;//false
+var e = 5 == 5;//true
+var f = 4 != 4;//false
+var g = 7 == '7';//ture
+var h = 7 === '7';//false
+var i = 7 === 7;//ture
+document.write(' Resposta = ' + i)
+
+
